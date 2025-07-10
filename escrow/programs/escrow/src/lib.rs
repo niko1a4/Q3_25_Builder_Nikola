02c5a977956a772ce1599a148a91573f7d4e653e
n1kol4a <andreevnikola200247@gmail.com>
2025-07-10 17:47:56 +0200

    Add escrow

---
+++ escrow/programs/escrow/src/lib.rs
@@ -0,0 +1,27 @@
+#![allow(unexpected_cfgs, deprecated)]
+use anchor_lang::prelude::*;
+
+pub mod instructions;
+pub mod state;
+
+use instructions::*;
+use state::*;
+
+declare_id!("5p1mnMcRMBBrPFgmEtGbXDyjThWNH31TeY3TgEELocFh");
+
+#[program]
+pub mod anchor_escrow {
+    use super::*;
+
+    pub fn make(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)?;
+        Ok(())
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close()?;
+        Ok(())
+    }
+}
